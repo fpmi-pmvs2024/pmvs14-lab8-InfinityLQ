5ae2b1a5b3f05b308c977b134ed78dd215e67e0a
InfinityLQ <3035763773@qq.com>
2024-05-18 16:48:54 +0300

    Add flag quiz game

---
+++ TaskFlagQuiz/app/src/main/java/com/example/taskflagquiz/CountryItem.java
@@ -0,0 +1,18 @@
+package com.example.taskflagquiz;
+
+public class CountryItem {
+    String name;
+    int image;
+    public  CountryItem(String name,int image){
+        this.name=name;
+        this.image=image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
---
+++ TaskFlagQuiz/app/src/main/java/com/example/taskflagquiz/Database.java
@@ -0,0 +1,39 @@
+package com.example.taskflagquiz;
+
+public class Database {
+    Integer[] flags = {
+            R.drawable.china,
+            R.drawable.belarus,
+            R.drawable.canada,
+            R.drawable.uk,
+            R.drawable.germany,
+            R.drawable.japan,
+            R.drawable.russia,
+            R.drawable.hongkong,
+            R.drawable.iceland,
+            R.drawable.usa,
+            R.drawable.ukraine,
+            R.drawable.holland,
+            R.drawable.elsalvador,
+            R.drawable.sierraleone
+
+    };
+
+    String[] answers = {
+            "china",
+            "belarus",
+            "canada",
+            "uk",
+            "germany",
+            "japan",
+            "russia",
+            "hongkong",
+            "iceland",
+            "usa",
+            "ukraine",
+            "holland",
+            "elsalvador",
+            "sierraleone",
+
+    };
+}
